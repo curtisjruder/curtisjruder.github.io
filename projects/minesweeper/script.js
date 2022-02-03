748c4e21d514875befe915d14b565e1c14a201b5
Curtis Ruder HP <curtisjruder@gmail.com>
2022-02-02 22:13:46 -0600

    changed the minesweeper count

--- projects/minesweeper/script.js
+++ projects/minesweeper/script.js
@@ -44,7 +44,7 @@ function clearGridCells(){
 function generateSolution(){
     var cnt = 0;
     var cellCnt = document.getElementsByClassName('cell').length;
-    var maxCnt = Math.round((1+Math.random())*0.1*cellCnt);
+    var maxCnt = Math.round((1.2+Math.random()*.4)*0.1*cellCnt);
     minefield = [];
     
     do{            
